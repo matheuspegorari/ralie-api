ebe39df492d6cbc58519ad4947513b4161466567
Matheus Pegorari <pegorari42@gmail.com>
2025-04-26 17:58:01 -0300

    initial commit

---
+++ src/main/kotlin/dev/pegorari/boltenergy/ralieapi/RalieApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.pegorari.boltenergy.ralieapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RalieApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RalieApiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/pegorari/boltenergy/ralieapi/RalieApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.pegorari.boltenergy.ralieapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RalieApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
