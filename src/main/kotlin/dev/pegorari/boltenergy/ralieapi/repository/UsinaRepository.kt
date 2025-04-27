ca687a538e1bc42129713dc6b83d97228d7367a5
Matheus Pegorari <pegorari42@gmail.com>
2025-04-27 18:51:05 -0300

    refactor: changed way too fetch top Usinas

--- src/main/kotlin/dev/pegorari/boltenergy/ralieapi/repository/UsinaRepository.kt
+++ src/main/kotlin/dev/pegorari/boltenergy/ralieapi/repository/UsinaRepository.kt
@@ -1,10 +1,22 @@
 package dev.pegorari.boltenergy.ralieapi.repository
 
 import dev.pegorari.boltenergy.ralieapi.model.Usina
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UsinaRepository : JpaRepository<Usina, String> {
-    fun findTop5ByOrderByPotenciaOutorgadaKwDesc(): List<Usina>
+    @Query(value = """
+        WITH top_plants AS (
+            SELECT DISTINCT ON (cod_ceg) *
+            FROM usina
+            ORDER BY cod_ceg, potencia_outorgada_kw DESC
+        )
+        SELECT * FROM top_plants
+        ORDER BY potencia_outorgada_kw DESC
+        LIMIT 5
+    """, nativeQuery = true)
+    fun findTop5DistinctUsinasByCodCEG(): List<Usina>
 }
